36b641d97ccfe871d552717a443f6baef3f71714
nikolaiandrov <nikolaiandrov@abv.bg>
2024-12-03 23:19:34 +0200

    trying to add solution folder

--- Stealer/StartUp.cs
+++ Stealer/StartUp.cs
@@ -6,7 +6,6 @@
         {
             Spy spy = new Spy();
             string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
-
             Console.WriteLine(result);
         }
     }
