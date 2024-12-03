7829f6a03d48b65a36c911067f17110f694584b1
nikolaiandrov <nikolaiandrov@abv.bg>
2024-12-03 22:55:43 +0200

    Add project files.

---
+++ Stealer/Spy.cs
@@ -0,0 +1,37 @@
+﻿namespace Stealer
+{
+    using System.Reflection;
+    using System.Text;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldsName)
+        {
+            Type type = Type.GetType(className)!;
+
+            if (type == null )
+            {
+                return "";
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Class under investigation: {type.FullName}");
+
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic
+                | BindingFlags.Instance | BindingFlags.Static);  
+
+            object instance = Activator.CreateInstance(type, new object[] { })!;
+
+            foreach (var field in fields)
+            {
+                if (fieldsName.Contains(field.Name))
+                {
+                    sb.AppendLine($"{field.Name} = {field.GetValue(instance)}");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
