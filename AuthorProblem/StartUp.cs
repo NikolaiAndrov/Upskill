0e91ec78195420b302a9e7dc969511e8d2b64d87
nikolaiandrov <nikolaiandrov@abv.bg>
2024-12-05 22:38:07 +0200

    Tracker implemented

--- AuthorProblem/StartUp.cs
+++ AuthorProblem/StartUp.cs
@@ -1,7 +1,11 @@
-﻿public class StartUp
+﻿using AuthorProblem;
+
+public class StartUp
 {
+    [Author("Niki")]
     private static void Main(string[] args)
     {
-
+        Tracker tracker = new Tracker();
+        tracker.PrintMethodsByAuthor();
     }
 }
---
+++ AuthorProblem/Tracker.cs
@@ -0,0 +1,28 @@
+﻿namespace AuthorProblem
+{
+    using System.Reflection;
+
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type[] types = Assembly.GetExecutingAssembly().GetTypes();
+
+            foreach (var currentType in types)
+            {
+                MethodInfo[] methods = currentType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic 
+                    | BindingFlags.Instance | BindingFlags.Static);
+
+                foreach (var method in methods)
+                {
+                    var attribute = method.GetCustomAttribute<AuthorAttribute>();
+
+                    if (attribute != null)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attribute.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
