adf16c44595c3c33aca30a12b8a09dda31cd37c0
nikolaiandrov <nikolaiandrov@abv.bg>
2024-12-08 19:47:57 +0200

    Hello command implemented

---
+++ ReflectionAndAttributes/CommandPattern/Core/Models/HelloCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace CommandPattern.Core.Models
+{
+    using System;
+
+    public class HelloCommand : Contracts.ICommand
+    {
+        public string Execute(string[] args)
+        {
+            return $"Hello {args[0]}";
+        }
+    }
+}
