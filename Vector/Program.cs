0902ab858a2752ab6a8f1c2d011a6dac364a1143
nikolaiandrov <nikolaiandrov@abv.bg>
2024-12-18 21:54:28 +0200

    vector recursion

---
+++ Vector/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Vector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+            GenerateVector(vector, 0);
+        }
+
+        static void GenerateVector(int[] vector, int index)
+        {
+            if (index == vector.Length)
+            {
+                for (int i = 0; i < vector.Length; i++)
+                {
+                    Console.Write(vector[i]);
+                }
+                Console.WriteLine();
+                return;
+            }
+
+            vector[index] = 0;
+            GenerateVector(vector, index + 1);
+
+            vector[index] = 1;
+            GenerateVector(vector, index + 1);
+        }
+    }
+}
