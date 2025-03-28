5da6fa6912d4bdec5d92af5df38715dbcd5afaa2
nikolaiandrov <nikolaiandrov@abv.bg>
2025-03-28 17:12:40 +0200

    testing functionality

--- DesignPatterns/MementoPattern/Program.cs
+++ DesignPatterns/MementoPattern/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Editor editor = new Editor();
+            History history = new History(editor);
+            history.Backup();
+            Console.WriteLine(history.ShowHistory());
+
+            editor.Title = "Test";
+            editor.Content = "Testing Functionality";
+            history.Backup();
+            Console.WriteLine(history.ShowHistory());
+
+            history.Undo();
+            Console.WriteLine(history.ShowHistory());
         }
     }
 }
