eb3d5e7471a32a5908cef9806fb300eb4e1e56f0
nikolaiandrov <nikolaiandrov@abv.bg>
2025-03-28 16:09:41 +0200

    Editor class created

---
+++ DesignPatterns/MementoPattern/Editor.cs
@@ -0,0 +1,10 @@
+﻿namespace MementoPattern
+{
+    // Originator
+    public class Editor
+    {
+        public string Title { get; set; } = null!;
+
+        public string Content { get; set; } = null!;
+    }
+}
