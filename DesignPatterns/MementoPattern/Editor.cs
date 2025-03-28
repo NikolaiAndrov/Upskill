166c700200070c1d659579d7a4a0f59f5a5c2fc1
nikolaiandrov <nikolaiandrov@abv.bg>
2025-03-28 16:23:55 +0200

    methods added in Editor

--- DesignPatterns/MementoPattern/Editor.cs
+++ DesignPatterns/MementoPattern/Editor.cs
@@ -6,5 +6,16 @@
         public string Title { get; set; } = null!;
 
         public string Content { get; set; } = null!;
+
+        public EditorState CreateState()
+        {
+            return new EditorState(this.Title, this.Content);
+        }
+
+        public void Restore(EditorState state)
+        {
+            this.Title = state.GetTitle();
+            this.Content = state.GetContent();
+        }
     }
 }
